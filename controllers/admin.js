78ba0ee165ce80c2a22b81bbe381606ea2814dbf
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-26 11:57:45 +0200

    use the database connection

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,35 +32,35 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-
-  if (!editMode) {
-    return res.redirect("/");
-  }
-
-  const prodId = req.params.productId;
-
-  req.user // find products for the currently logged in user
-    .getProducts({ where: { id: prodId } }) // always returns an array
-    //  Product.findByPk(prodId)
-    .then((products) => {
-      const product = products[0];
-
-      if (!product) {
-        return res.redirect("/");
-      }
-      res.render("admin/edit-product", {
-        pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//
+//   if (!editMode) {
+//     return res.redirect("/");
+//   }
+//
+//   const prodId = req.params.productId;
+//
+//   req.user // find products for the currently logged in user
+//     .getProducts({ where: { id: prodId } }) // always returns an array
+//     //  Product.findByPk(prodId)
+//     .then((products) => {
+//       const product = products[0];
+//
+//       if (!product) {
+//         return res.redirect("/");
+//       }
+//       res.render("admin/edit-product", {
+//         pageTitle: "Edit Product",
+//         path: "/admin/edit-product",
+//         editing: editMode,
+//         product: product,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
--- models/product.js
+++ models/product.js
@@ -1,29 +1,25 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../util/database");
+const getDb = require("../util/database").getDb;
 
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("products") // it's created when we insert data if it doesn't exist
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = Product;
